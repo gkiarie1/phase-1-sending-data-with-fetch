e8e0156f31eafe6c8280d4fcd681c9d2cdd57bf4
gkiarie1 <gkaris24@gmail.com>
2024-02-13 13:10:49 +0300

    DONE

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+function submitData(name, email) {
+    const userData = {
+      name: name,
+      email: email
+    };
+  
+    const configObject = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(userData)
+    };
+  
+    return fetch("http://localhost:3000/users", configObject)
+      .then(response => response.json())
+      .then(data => {
+        // Access the new id and append it to the DOM
+        document.body.innerHTML += `<p>${data.id}</p>`;
+      })
+      .catch(error => {
+        // Append the error message to the DOM
+        document.body.innerHTML += `<p>${error.message}</p>`;
+      });
+  }
+  
